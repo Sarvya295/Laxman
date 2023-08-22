f5b4c3772396bd1707be3ea38e44644e629e319f
SarveshRathod01 <141236814+SarveshRathod01@users.noreply.github.com>
2023-08-22 18:37:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,9 +42,9 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sarvesh:sarvesh@cluster0.1jxebiu.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "vjbotsaban")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjbots')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sarvesh1:sarvesh1@cluster0.hra621a.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "sarvesh1")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'sarvesh1')
 
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'moneykamalo.com')
